457ae4f5674ad56b52fe040acaed5af2d7348d3a
Rhuan <pccardozo2911@gmail.com>
2026-01-09 11:40:25 -0300

    Layout

---
+++ src/Components/Car.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export default function Car () {
+    return (
+        <>
+            <Container>
+
+            </Container>
+        </>
+    )
+}
+
+const Container = styled.div`
+width: 400px;
+height: 100%;
+background-color: aqua;
+`
